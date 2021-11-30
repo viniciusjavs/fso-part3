c2b65d4d252c21810c15e904f3b86d8d5466efb8
Vinicius Sa <viniciusjavs@gmail.com>
2021-11-30 16:21:42 -0300

    Phonebook: Add mongodb services: add, list

---
+++ mongo.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose'
+
+if (process.argv.length < 3 || process.argv.length > 5) {
+    console.log('Program usage:')
+    console.log('To add a new entry: node mongo.js <password> <name> <number>')
+    console.log('To show all entries: node mongo.js <password>')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+    `mongodb+srv://app:${password}@fso.a1ete.mongodb.net/fso?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3) {
+    Person
+        .find({})
+        .then(res => {
+            console.log('phonebook:')
+            res.forEach(p => {
+                console.log(p)
+            })
+            mongoose.connection.close()
+        })
+}
+else {
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4]
+    })
+    person
+        .save()
+        .then(res => {
+            console.log(`added ${person.name} number ${person.number} to phonebook`)
+            mongoose.connection.close()
+        })
+}
