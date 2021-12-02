bfb7ea00cd23869487a886694519a9290951e5da
Vinicius Sa <viniciusjavs@gmail.com>
2021-12-01 23:22:45 -0300

    Phonebook: add eslint

--- models/person.js
+++ models/person.js
@@ -9,33 +9,33 @@ console.log('connecting to', url)
 url = url.replace('MONGODB_PASS', process.env.MONGODB_PASS)
 
 mongoose
-    .connect(url)
-    .then(res => console.log('connected to MongoDB'))
-    .catch(({ message }) => console.log('error connecting to MongoDB: ', message))
+  .connect(url)
+  .then(() => console.log('connected to MongoDB'))
+  .catch(({ message }) => console.log('error connecting to MongoDB: ', message))
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength: 3,
-        required: true,
-        unique: true
-    },
-    number: {
-        type: String,
-        minlength: 9,
-        maxlength: 11,
-        required: true,
-        match: /^[0-9]{3}-[0-9]{5,7}$/
-    }
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+    unique: true
+  },
+  number: {
+    type: String,
+    minlength: 9,
+    maxlength: 11,
+    required: true,
+    match: /^[0-9]{3}-[0-9]{5,7}$/
+  }
 })
 personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
-    transform: (_document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (_document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 const Person = mongoose.model('Person', personSchema)
--- mongo.js
+++ mongo.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose'
 
 if (process.argv.length < 3 || process.argv.length > 5) {
-    console.log('Program usage:')
-    console.log('To add a new entry: node mongo.js <password> <name> <number>')
-    console.log('To show all entries: node mongo.js <password>')
-    process.exit(1)
+  console.log('Program usage:')
+  console.log('To add a new entry: node mongo.js <password> <name> <number>')
+  console.log('To show all entries: node mongo.js <password>')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -15,32 +15,32 @@ const url =
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+  name: String,
+  number: String
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length === 3) {
-    Person
-        .find({})
-        .then(res => {
-            console.log('phonebook:')
-            res.forEach(p => {
-                console.log(p)
-            })
-            mongoose.connection.close()
-        })
+  Person
+    .find({})
+    .then(res => {
+      console.log('phonebook:')
+      res.forEach(p => {
+        console.log(p)
+      })
+      mongoose.connection.close()
+    })
 }
 else {
-    const person = new Person({
-        name: process.argv[3],
-        number: process.argv[4]
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4]
+  })
+  person
+    .save()
+    .then(() => {
+      console.log(`added ${person.name} number ${person.number} to phonebook`)
+      mongoose.connection.close()
     })
-    person
-        .save()
-        .then(res => {
-            console.log(`added ${person.name} number ${person.number} to phonebook`)
-            mongoose.connection.close()
-        })
 }
